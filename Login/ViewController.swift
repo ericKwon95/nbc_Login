8d69f77bb0bd891f6b55c212fb94b9d8f46e0c6e
ericKwon95 <ericyong95@gmail.com>
2025-03-13 16:02:30 +0900

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by 권승용 on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoginTests/LoginTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTests.swift
+//  LoginTests
+//
+//  Created by 권승용 on 3/13/25.
+//
+
+import Testing
+@testable import Login
+
+struct LoginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
