56eddd93c74823a3bc3e6d480846cc10977d406b
ericKwon95 <ericyong95@gmail.com>
2025-03-14 16:20:16 +0900

    [Feat] 시작하기 뷰 커스텀 버튼 적용 / SwiftFormat 적용

--- Login/Source/Feature/Start/StartViewController.swift
+++ Login/Source/Feature/Start/StartViewController.swift
@@ -5,32 +5,43 @@
 //  Created by 권승용 on 3/14/25.
 //
 
-import UIKit
 import SnapKit
+import UIKit
 
 final class StartViewController: UIViewController {
-    private lazy var startView = StartView() { [weak self] in
+    // MARK: - Properties
+
+    private lazy var startView = StartView { [weak self] in
         self?.startViewTapped()
     }
-    
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureBackgound()
         configureHierarchy()
         configureLayout()
     }
-    
+
+    // MARK: - Functions
+
+    private func configureBackgound() {
+        view.backgroundColor = .appBackground
+    }
+
     private func configureHierarchy() {
         [
-            startView
+            startView,
         ].forEach { view.addSubview($0) }
     }
-    
+
     private func configureLayout() {
         startView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
     }
-    
+
     private func startViewTapped() {
         print("startViewTapped2")
     }
