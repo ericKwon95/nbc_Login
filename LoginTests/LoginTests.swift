cbe0fdc0881db405a2ae9e88fd5402c734ae7e95
ericKwon95 <ericyong95@gmail.com>
2025-03-14 16:06:20 +0900

    [Chore] SwiftFormat 적용

--- LoginTests/LoginTests.swift
+++ LoginTests/LoginTests.swift
@@ -5,13 +5,12 @@
 //  Created by 권승용 on 3/13/25.
 //
 
-import Testing
 @testable import Login
+import Testing
 
 struct LoginTests {
-
-    @Test func example() async throws {
+    @Test
+    func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
