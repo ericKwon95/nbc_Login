b6941cd1fbaabd43e67fe764cb0be7103ec37534
ericKwon95 <ericyong95@gmail.com>
2025-03-16 22:28:54 +0900

    [Test] 변경한 이메일 검증 로직에 맞춰 테스트코드 업데이트

--- LoginTests/ServiceTests/SignUpValidationServiceTests/EmailValidationTests.swift
+++ LoginTests/ServiceTests/SignUpValidationServiceTests/EmailValidationTests.swift
@@ -36,7 +36,7 @@ struct EmailValidationTests {
     ])
     func validateEmailLength(_ email: String) {
         let result = service.validateEmail(email)
-        #expect(result == .invalid)
+        #expect(result == .invalidLength)
     }
 
     @Test("공백 포함 이메일 실패 테스트", arguments: [
@@ -54,24 +54,39 @@ struct EmailValidationTests {
     ])
     func validateInvalidEmailWithInvalidCharacter(_ email: String) {
         let result = service.validateEmail(email)
-        #expect(result == .invalid)
+        #expect(result == .invalidCharacter)
     }
 
     @Test("유효하지 않은 이메일 형식 실패 테스트", arguments: [
-        "123test@example.com", // 숫자로 시작
         "@example.com", // 로컬 파트 없음
-        "test@", // 도메인 없음
-        "test@invalid-domain", // 잘못된 도메인
-        "test@.com", // 도메인 이름 없음
-        "test@example.", // 최상위 도메인 없음
+        "testtt@", // 도메인 없음
+
     ])
     func validateEmailFormat(_ email: String) {
         let result = service.validateEmail(email)
         #expect(result == .invalid)
     }
 
+    @Test("유효하지 않은 도메인 실패 테스트", arguments: [
+        "testtt@invalid-domain", // 잘못된 도메인
+        "testtt@.com", // 도메인 이름 없음
+        "testtt@example.", // 최상위 도메인 없음
+    ])
+    func validateInvalidDomain(_ email: String) {
+        let result = service.validateEmail(email)
+        #expect(result == .invalidDomain)
+    }
+
+    @Test("숫자로 시작하는 이메일 실패 테스트", arguments: [
+        "123abc@example.com", // 숫자로 시작
+    ])
+    func validateEmailStartingWithNumber(_ email: String) {
+        let result = service.validateEmail(email)
+        #expect(result == .startsWithNumber)
+    }
+
     @Test("빈 이메일 실패 테스트", arguments: [
-        "",
+        "", // 공백
     ])
     func validateEmptyEmail(_ email: String) {
         let result = service.validateEmail(email)
