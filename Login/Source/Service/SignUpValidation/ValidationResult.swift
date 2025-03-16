6ced3f07efa1fe53e1d509f5756a5696eb688820
ericKwon95 <ericyong95@gmail.com>
2025-03-16 14:51:50 +0900

    [Feat] 공백 포함 / 중복 검증

--- Login/Source/Service/SignUpValidation/ValidationResult.swift
+++ Login/Source/Service/SignUpValidation/ValidationResult.swift
@@ -15,7 +15,9 @@ protocol ValidationResult {
 enum EmailValidationResult: ValidationResult {
     case valid // 유효한 이메일
     case invalid // 유효하지 않은 이메일
+    case invalidWhitespace // 공백 포함
     case empty // 빈 문자열
+    case duplicated // 중복
 
     // MARK: - Computed Properties
 
@@ -29,8 +31,12 @@ enum EmailValidationResult: ValidationResult {
             return "올바른 이메일 양식 입니다."
         case .invalid:
             return "올바르지 않은 이메일 양식 입니다."
+        case .invalidWhitespace:
+            return "이메일은 공백을 포함할 수 없습니다."
         case .empty:
             return "이메일을 입력해 주세요"
+        case .duplicated:
+            return "중복된 이메일 입니다."
         }
     }
 }
@@ -38,6 +44,7 @@ enum EmailValidationResult: ValidationResult {
 enum PasswordValidationResult: ValidationResult {
     case valid // 유효한 비밀번호
     case invalidLength // 8자 미만 24자 초과
+    case invalidWhitespace // 공백 포함
     case noUppercase // 대문자 없음
     case noSpecialCharacter // 특수문자 없음
     case noNumber // 숫자 없음
@@ -55,6 +62,8 @@ enum PasswordValidationResult: ValidationResult {
             return "유효한 비밀번호 입니다."
         case .invalidLength:
             return "비밀번호는 8자 이상 24자 이하로 입력해주세요."
+        case .invalidWhitespace:
+            return "비밀번호는 공백을 포함할 수 없습니다."
         case .noUppercase:
             return "비밀번호는 최소 하나 이상의 대문자를 포함해야 합니다."
         case .noSpecialCharacter:
@@ -90,6 +99,7 @@ enum ConfirmPasswordValidationResult: ValidationResult {
 enum NicknameValidationResult: ValidationResult {
     case valid // 유효하인 닉네임
     case invalidLength // 길이 조건 불만족 (4-12자)
+    case invalidWhitespace // 공백 포함
     case invalidCharacter // 특수문자 포함
     case empty // 빈 문자열
 
@@ -105,6 +115,8 @@ enum NicknameValidationResult: ValidationResult {
             return "유효한 닉네임입니다."
         case .invalidLength:
             return "닉네임은 4자 이상 12자 이하로 입력해주세요."
+        case .invalidWhitespace:
+            return "닉네임은 공백을 포함할 수 없습니다."
         case .invalidCharacter:
             return "닉네임은 특수문자를 포함할 수 없습니다."
         case .empty:
