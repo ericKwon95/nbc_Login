698a7279dfc9a42ad053c5cb5fee915ccfcf35a1
ericKwon95 <ericyong95@gmail.com>
2025-03-16 15:43:22 +0900

    [Feat] 회원가입 뷰 관련 네비게이션 구현

--- Login/App/SceneDelegate.swift
+++ Login/App/SceneDelegate.swift
@@ -27,18 +27,29 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
 
         let keychainManager = KeychainManager()
+        let coreDataManager = CoreDataManager.shared
         let loginKeychainStorage = LoginKeychainStorage(keychainManager: keychainManager)
+
         let loginLandingViewModel =
             LoginLandingViewModel(loginKeychainStorage: loginKeychainStorage)
         let loginLandingViewController =
             LoginLandingViewController(viewModel: loginLandingViewModel)
+
+        let userRepository = DefaultUserRepository(coredataManager: coreDataManager)
+        let validationService = SignUpValidationService(repository: userRepository)
+        let signUpViewModel = SignUpViewModel(
+            userRepository: userRepository,
+            validationService: validationService,
+            loginKeychainStorage: loginKeychainStorage
+        )
+        let signUpViewController = SignUpViewController(viewModel: signUpViewModel)
+
         let navigationController =
             UINavigationController(rootViewController: loginLandingViewController)
 
         loginLandingViewModel.navigateToSignUp
             .observe(on: MainScheduler.instance)
             .subscribe(with: self) { _, _ in
-                let signUpViewController = SignUpViewController()
                 navigationController.pushViewController(signUpViewController, animated: true)
             }
             .disposed(by: disposeBag)
@@ -48,6 +59,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .subscribe(with: self) { _, _ in
                 let loginSuccessViewController = LoginSuccessViewController()
                 navigationController.pushViewController(loginSuccessViewController, animated: true)
+            }
+            .disposed(by: disposeBag)
+
+        signUpViewModel.navigateToLoginSuccess
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { _, _ in
+                navigationController.pushViewController(
+                    LoginSuccessViewController(),
+                    animated: true
+                )
             }
             .disposed(by: disposeBag)
 
