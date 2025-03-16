cc65934c3887223ccbf1a4afc67f4ddeb55c2bb0
ericKwon95 <ericyong95@gmail.com>
2025-03-16 17:37:51 +0900

    [Feat] 키체인에 Bool값 아닌 email 저장
    - 재로그인 시 어떤 유저인지 확인을 위해 변경

--- Login/Source/Persistence/LoginKeychainStorage.swift
+++ Login/Source/Persistence/LoginKeychainStorage.swift
@@ -10,7 +10,7 @@ struct LoginKeychainStorage: LoginKeychainStorageable {
     // MARK: - Properties
 
     /// 키체인에서 로그인 상태를 저장하는 키 값
-    private let isLoggedInKey = "isLoggedIn"
+    private let userLoginKey = "emailLoggedIn"
     /// 키체인 관리 객체
     private let keychainManager: KeychainManageable
 
@@ -27,29 +27,30 @@ struct LoginKeychainStorage: LoginKeychainStorageable {
     /// 로그인 상태를 키체인에 저장
     /// - Parameter isLoggedIn: 로그인 상태 (true: 로그인됨, false: 로그아웃됨)
     /// - Throws: 키체인 저장 중 발생할 수 있는 오류
-    func setIsLoggedIn(_ isLoggedIn: Bool) async throws {
-        let data = String(isLoggedIn).data(using: .utf8)!
+    func setUserEmail(with email: String) async throws {
+        let data = email.data(using: .utf8)!
 
         // 기존 데이터가 있다면 업데이트, 없다면 저장
-        if try await getIsLoggedIn() {
-            try await keychainManager.update(data, forKey: isLoggedInKey)
-        } else {
-            try await keychainManager.save(data, forKey: isLoggedInKey)
+        do {
+            _ = try await getUserEmail()
+            try await keychainManager.update(data, forKey: userLoginKey)
+        } catch {
+            try await keychainManager.save(data, forKey: userLoginKey)
         }
     }
 
     /// 키체인에서 로그인 상태를 조회
     /// - Returns: 로그인 상태 (true: 로그인됨, false: 로그아웃됨)
     /// - Throws: 키체인 읽기 중 발생할 수 있는 오류
-    func getIsLoggedIn() async throws -> Bool {
-        do {
-            guard let data = try await keychainManager.read(forKey: isLoggedInKey),
-                  let stringValue = String(data: data, encoding: .utf8) else {
-                return false
-            }
-            return stringValue == "true"
-        } catch {
-            return false
+    func getUserEmail() async throws -> String {
+        guard let data = try await keychainManager.read(forKey: userLoginKey) else {
+            throw KeychainError.itemNotFound
         }
+        return String(data: data, encoding: .utf8)!
+    }
+
+    /// 키체인에서 로그인 상태를 삭제
+    func removeUserEmail() async throws {
+        try await keychainManager.delete(forKey: userLoginKey)
     }
 }
